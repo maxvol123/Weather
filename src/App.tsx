c9698fa9b0dbe0509c9df0447b50ee302d436352
Maksym Voloshyn <maxvol200727@gmail.com>
2022-08-23 15:50:32 +0300

    Add Data and time

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,38 @@ let filterValue =Wheather_element.filter(weather =>{
 })
     console.log(filterValue);
     console.log(value);
-    
+    function getDay() {
+      if (new Date().getDay()===1) {
+        return "Monday"
+      }
+      if (new Date().getDay()===2) {
+        return "Tuesday"
+      }
+      if (new Date().getDay()===3) {
+        return "Wednesday"
+      }
+      if (new Date().getDay()===4) {
+        return "Thursday"
+      }
+      if (new Date().getDay()===5) {
+        return "Friday"
+      }
+      if (new Date().getDay()===6) {
+        return "Saturday"
+      }
+      if (new Date().getDay()===0) {
+        return "Sunday"
+      }
+    }
+  
     
 
   return (
     <>
+    <div className="time">
+     <div className="text-white text-5xl">{getDay()}</div>
+    <div className="text-white text-4xl">{new Date().getHours()}:{new Date().getMinutes()}</div>
+    </div>
     <div className="">
       <input className='text-white rounded border-none' type="text" placeholder='Enter city'
       value={value}
--- src/components/Create_element.tsx
+++ src/components/Create_element.tsx
@@ -10,26 +10,26 @@ export function CreateElement(element: ElementProps){
     function weather_foto() {
         if (element.Wheather_element.wheather==="Sunny") {
             return(
-                <img className=" z-5 w-20 h-20 absolute" src={Sun} alt="" />
+                <img className=" z-2 w-20 h-20 absolute" src={Sun} alt="" />
             )
         }
         if (element.Wheather_element.wheather==="Rain") {
             return(
-                <img className=" z-5 w-20 h-20 absolute" src={Rain} alt="" />
+                <img className=" z-2 w-20 h-20 absolute" src={Rain} alt="" />
             )
         }
         if (element.Wheather_element.wheather==="Cloud") {
             return(
-                <img className=" z-5 w-20 h-20 absolute" src={Cloud} alt="" />
+                <img className=" z-2 w-20 h-20 absolute" src={Cloud} alt="" />
             )
         }
     }
     return(
-        <div className="border py-2 rounded flex flex-col items-center mb-2 z-0 w-80 mr-3 text-white" key={element.Wheather_element.id}>
-            <div className="mr-auto select-none">{weather_foto()}</div>
-            <div className="z-0">{element.Wheather_element.City}</div>
-            <div className="z-0">{element.Wheather_element.wheather} </div>
-            <div className="z-0">{element.Wheather_element.temperature}°C</div>
+        <div className="border py-2 rounded flex flex-col items-center mb-2 z-2 w-80 mr-3 text-white mt-3 bg-black/30" key={element.Wheather_element.id}>
+            <div className="z-2 mr-auto select-none">{weather_foto()}</div>
+            <div className="z-2">{element.Wheather_element.City}</div>
+            <div className="z-2">{element.Wheather_element.wheather} </div>
+            <div className="z-2">{element.Wheather_element.temperature}°C</div>
         </div>
     )
 }
